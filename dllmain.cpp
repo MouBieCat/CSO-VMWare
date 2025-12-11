a9ffda1f4cc004db006a5aeddedc4bc01247ec55
hongyi.jian <hongyi.jian@gigabyte.com>
2025-12-11 10:21:32 +0800

    添加相對應的入口點函數

---
+++ dllmain.cpp
@@ -0,0 +1,5 @@
+#include <Windows.h>
+
+BOOL WINAPI DllMain(HINSTANCE _Instance, DWORD _Reason, LPVOID _Reserved) {
+    return TRUE;
+}
---
+++ main.cpp
@@ -0,0 +1,5 @@
+#include <Windows.h>
+
+INT WINAPI WinMain(HINSTANCE _Instance, HINSTANCE _PrevInstance, PWSTR _CmdLine, INT _CmdShow) {
+	return 0;
+}
