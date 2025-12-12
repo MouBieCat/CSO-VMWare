e256f271d64c6108684c156b515a3b60f4fd9cbd
moubiecat <itsmoubiecat@gmail.com>
2025-12-13 03:38:20 +0800

    添加簡單的 DllMain 流程

--- dllmain.cpp
+++ dllmain.cpp
@@ -1,5 +1,15 @@
 #include <Windows.h>
 
 BOOL WINAPI DllMain(HINSTANCE _Instance, DWORD _Reason, LPVOID _Reserved) {
+    switch (_Reason) {
+    case DLL_PROCESS_ATTACH:
+        // Initialize once for each new process.
+        break;
+
+    case DLL_PROCESS_DETACH:
+        // Perform any necessary cleanup.
+        break;
+    }
+
     return TRUE;
 }
